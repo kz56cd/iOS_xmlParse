298dd30e162ec0f2577ee929548c52671107e4dd
kz56cd <kz56cd@gmail.com>
2015-07-22 15:57:16 +0900

    Initial Commit

---
+++ XMLParse01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XMLParse01
+//
+//  Created by 業務委託スタッフ on 2015/07/22.
+//  Copyright (c) 2015年 msano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
